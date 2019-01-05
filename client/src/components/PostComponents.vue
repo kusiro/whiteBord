364f68148bd348fa8ef271ac7bf8ed87198d6ba8
kusiro <jackysoon871008@gmail.com>
2019-01-05 21:21:52 +0800

    Delete function

--- client/src/components/PostComponents.vue
+++ client/src/components/PostComponents.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <h1>Latest Posts</h1>
-    <!-- Create Post Here -->
+    <div class="create-post">
+      <label for="create-post">Say Someting...</label>
+      <input type="text" id="create-post" v-model="text" placeholder="Create a post">
+      <button v-on:click="createPost">Post</button>
+    </div>
     <hr>
     <p class="error" v-if="error">{{ error }}</p>
     
@@ -10,7 +14,8 @@
           v-for="(post, index) in posts"
           v-bind:item="post"
           v-bind:index="index"
-          v-bind:key="post._id">
+          v-bind:key="post._id"
+          v-on:dblclick="deletePost(post._id)">
             {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}` }}
             <p class="text">{{ post.text }}</p>
 
@@ -36,6 +41,16 @@ export default {
     } catch(err){
       this.error = err.message;
     }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+    },
+    async deletePost(id) {
+      await PostService.deletePost("/"+id);
+      this.posts = await PostService.getPosts();
+    }
   }
 }
 </script>
@@ -47,12 +62,12 @@ h3 {
 }
 .posts-container {
   max-width: 700px;
-  margin: 0 auto;
 }
 .post {
   background-color: #066259;
   color: white;
   padding: 1em 0;
+  margin: 0 auto 1em auto;
 }
 .text {
   color: white;
